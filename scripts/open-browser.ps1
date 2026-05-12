79f0244875758b1154b5967ffaffa735a118e511
CESARE SALERNO <cesare.salerno@emeal.nttdata.com>
2026-05-12 14:46:34 +0200

    Configure VS Code Chrome launch

--- scripts/open-browser.ps1
+++ scripts/open-browser.ps1
@@ -1,6 +1,11 @@
+param(
+    [string]$Url = "http://127.0.0.1:8000",
+    [int]$TimeoutSeconds = 45,
+    [int]$PollMilliseconds = 500
+)
+
 $ErrorActionPreference = "Stop"
 
-$url = "http://127.0.0.1:8000"
 $chromeCandidates = @(
     "$env:ProgramFiles\Google\Chrome\Application\chrome.exe",
     "${env:ProgramFiles(x86)}\Google\Chrome\Application\chrome.exe",
@@ -9,8 +14,54 @@ $chromeCandidates = @(
 
 $chrome = $chromeCandidates | Where-Object { Test-Path -LiteralPath $_ } | Select-Object -First 1
 
-if ($chrome) {
-    Start-Process -FilePath $chrome -ArgumentList $url
-} else {
-    Start-Process $url
+if (-not $chrome) {
+    $chromeCommand = Get-Command chrome.exe -ErrorAction SilentlyContinue
+    if ($chromeCommand) {
+        $chrome = $chromeCommand.Source
+    }
 }
+
+if (-not $chrome) {
+    throw "Chrome non trovato. Installa Google Chrome o aggiungi chrome.exe al PATH."
+}
+
+$launcher = {
+    param(
+        [string]$TargetUrl,
+        [string]$ChromePath,
+        [int]$WaitSeconds,
+        [int]$SleepMilliseconds
+    )
+
+    $ErrorActionPreference = "SilentlyContinue"
+    $deadline = (Get-Date).AddSeconds($WaitSeconds)
+
+    do {
+        $response = Invoke-WebRequest -Uri $TargetUrl -UseBasicParsing -TimeoutSec 1
+        if ($response.StatusCode -ge 200 -and $response.StatusCode -lt 500) {
+            break
+        }
+
+        Start-Sleep -Milliseconds $SleepMilliseconds
+    } while ((Get-Date) -lt $deadline)
+
+    Start-Process -FilePath $ChromePath -ArgumentList $TargetUrl
+}
+
+$escapedUrl = $Url.Replace("'", "''")
+$escapedChrome = $chrome.Replace("'", "''")
+$command = @"
+& {
+$launcher
+} -TargetUrl '$escapedUrl' -ChromePath '$escapedChrome' -WaitSeconds $TimeoutSeconds -SleepMilliseconds $PollMilliseconds
+"@
+
+$encodedCommand = [Convert]::ToBase64String([Text.Encoding]::Unicode.GetBytes($command))
+
+Start-Process -FilePath "powershell" -ArgumentList @(
+    "-NoProfile",
+    "-ExecutionPolicy",
+    "Bypass",
+    "-EncodedCommand",
+    $encodedCommand
+) -WindowStyle Hidden
